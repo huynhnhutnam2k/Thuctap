3902ec4dca7aaa0b7c4e10097a9a4d1e766deafa
hnam <hnam4527@gmail.com>
2022-05-27 21:50:21 +0700

    up

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json())
 
 router(app)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log('App listening at: http://localhost:' + port)
 })
