db7194ae4a121aa5d9c4de14317b42d567dd65e4
congtoai <toai.tit0203@gmail.com>
2022-06-29 00:21:32 +0700

    css

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,52 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
-import { getAllDepartment } from '../../redux/departmentSlice';
+import { getAllDepartment } from "../../redux/departmentSlice";
 function Nav() {
   const dispatch = useDispatch();
-  const { listDepartment: department } = useSelector((state) => state.department)
-  const { pathname } = useLocation()
+  const { listDepartment: department } = useSelector(
+    (state) => state.department
+  );
+  const { pathname } = useLocation();
   let navigate = useNavigate();
   const routeChange = (e) => {
     let path = e.target.value;
     navigate(path);
-  }
+  };
   useEffect(() => {
     dispatch(getAllDepartment());
-  }, [])
-  const [current, setCurret] = useState("")
+  }, []);
+  const [current, setCurret] = useState("");
   return (
     <div>
       <nav className="navbar navbar-expand-sm navbar-light bg">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
-            {department?.map(item => (
-              <div className="submenu nav-item" key={item._id} id={item._id} >
+            {department?.map((item) => (
+              <div className="submenu nav-item" key={item._id} id={item._id}>
                 <li className="nav-item">
                   <NavLink
-                    className={(navData) => (navData.isActive ? "nav-item active" : 'nav-item')}
-                    to={`/department/${item._id}`}>
+                    className={(navData) =>
+                      navData.isActive ? "nav-item active" : "nav-item"
+                    }
+                    to={`/department/${item._id}`}
+                  >
                     <span>{item.name}</span>
                   </NavLink>
                 </li>
               </div>
             ))}
 
-
             <div className="all">
-              <select name="sort" id="sort" onClick={routeChange} defaultValue={''}>
+              <select
+                name="sort"
+                id="sort"
+                onClick={routeChange}
+                defaultValue={""}
+              >
                 <option value="">-Chọn Khoa-</option>
-                {department?.map(item => (
-                  <option value={`/department/${item._id}`} key={item._id} >{item.name}</option>
+                {department?.map((item) => (
+                  <option value={`/department/${item._id}`} key={item._id}>
+                    {item.name}
+                  </option>
                 ))}
-                <option value={`/#`} >Tất cả</option>
+                <option value={`/#`}>Tất cả</option>
               </select>
             </div>
           </ul>
         </div>
-
-
       </nav>
     </div>
   );
--- src/components/situation/index.jsx
+++ src/components/situation/index.jsx
@@ -1,62 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Popup from './Popup';
-import parse from 'html-react-parser';
-import { getAllSituation } from '../../redux/situationSlice';
-import { getAllDepartment } from '../../redux/departmentSlice';
-import { getAllTreatment } from '../../redux/treatmentSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Popup from "./Popup";
+import parse from "html-react-parser";
+import { getAllSituation } from "../../redux/situationSlice";
+import { getAllDepartment } from "../../redux/departmentSlice";
+import { getAllTreatment } from "../../redux/treatmentSlice";
 // import queryString from 'query-string';
 
 function Situation() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [situationId, setSituationId] = useState("")
+  const [isOpen, setIsOpen] = useState(false);
+  const [situationId, setSituationId] = useState("");
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getAllSituation())
-    dispatch(getAllDepartment())
-    dispatch(getAllTreatment())
-  }, [dispatch])
-  const { listSituation: situation } = useSelector((state) => state.situation)
+    dispatch(getAllSituation());
+    dispatch(getAllDepartment());
+    dispatch(getAllTreatment());
+  }, [dispatch]);
+  const { listSituation: situation } = useSelector((state) => state.situation);
 
   const handleClick = (id) => {
     setIsOpen(true);
     setSituationId(id);
   };
 
-  // const [filters, setFilters] = useState({
-  //   _limit: 1,
-  //   _page: 1,
-  // });
-
-
-
-  // function handlePageChange(newPage){
-  //   console.log('New page: ', newPage );
-  //   setFilters({
-  //     ... filters,
-  //     _page: newPage,
-  //   })
-  // }
-
-
-
   const close = () => {
     setIsOpen(false);
     setSituationId("");
-  }
-  return (<div className="question col-12">
-    {situation?.map((item) => (
-      <div className="situation col-6 col-md-4 col-lg-2" value={item._id} key={item._id} onClick={() => handleClick(item._id)}>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8uRX4rai3Bv0Lb_B6XL0WCUbQ4i-QajaX8sgavBvkI7hSCC-5lBTAJBFKm7tX0AGzzlo&usqp=CAU" alt="img" />
-        <p><b>Tình huống:</b> {item.name}</p>
-        <p><b>Mô tả:</b> <span className="desc">{parse(item.desc)}</span></p>
-        <p><b>Điểm TB:</b> {item.averageMark}</p>
-      </div>
-    ))}
-    {/* POPUP_QUESTION */}
-    <Popup open={isOpen} id={situationId} onClose={close}></Popup>
-  </div>
-  )
+  };
+  return (
+    <div className="question col-12">
+      {situation?.map((item) => (
+        <div
+          className="situation col-6 col-md-4 col-lg-2"
+          value={item._id}
+          key={item._id}
+          onClick={() => handleClick(item._id)}
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8uRX4rai3Bv0Lb_B6XL0WCUbQ4i-QajaX8sgavBvkI7hSCC-5lBTAJBFKm7tX0AGzzlo&usqp=CAU"
+            alt="img"
+          />
+          <p>
+            <b>Tình huống:</b> {item.name}
+          </p>
+          <div className="desc">
+            <b>Mô tả:</b> {parse(item.desc)}
+          </div>
+        </div>
+      ))}
+      {/* POPUP_QUESTION */}
+      <Popup open={isOpen} id={situationId} onClose={close}></Popup>
+    </div>
+  );
 }
 
 export default Situation;
