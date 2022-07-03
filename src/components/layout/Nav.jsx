41492c776997462e86a0b7cf33c1f5a54bc52d2d
congtoai <toai.tit0203@gmail.com>
2022-07-04 01:55:58 +0700

    css

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -19,50 +19,47 @@ function Nav() {
   const [current, setCurret] = useState("");
   return (
     <div>
-      <nav className="navbar navbar-expand-sm navbar-light bg">
-        <button
+      <div className="menu-container">
+        <nav className="navbar navbar-expand-sm navbar-light bg">
+          {/* <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#collapsibleNavbar"
         >
           <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav">
-            {department?.map((item) => (
-              <div className="submenu nav-item" key={item._id} id={item._id}>
-                <li className="nav-item">
-                  <NavLink
-                    className={(navData) =>
-                      navData.isActive ? "nav-item active" : "nav-item"
-                    }
-                    to={`/department/${item._id}`}
-                  >
-                    <span>{item.name}</span>
-                  </NavLink>
-                </li>
-              </div>
-            ))}
-          </ul>
-          <div className="all">
-            <select
-              name="sort"
-              id="sort"
-              onClick={routeChange}
-              defaultValue={""}
-            >
-              <option value="">-Chọn Khoa-</option>
+        </button> */}
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
               {department?.map((item) => (
-                <option value={`/department/${item._id}`} key={item._id}>
-                  {item.name}
-                </option>
+                <div className="submenu nav-item" key={item._id} id={item._id}>
+                  <li className="nav-item">
+                    <NavLink
+                      className={(navData) =>
+                        navData.isActive ? "nav-item active" : "nav-item"
+                      }
+                      to={`/department/${item._id}`}
+                    >
+                      <span>{item.name}</span>
+                    </NavLink>
+                  </li>
+                </div>
               ))}
-              <option value={`/#`}>Tất cả</option>
-            </select>
+            </ul>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
+      <div className="all">
+        <select name="sort" id="sort" onClick={routeChange} defaultValue={""}>
+          <option value="">-Chọn Khoa-</option>
+          {department?.map((item) => (
+            <option value={`/department/${item._id}`} key={item._id}>
+              {item.name}
+            </option>
+          ))}
+          <option value={`/#`}>Tất cả</option>
+        </select>
+      </div>
     </div>
   );
 }
